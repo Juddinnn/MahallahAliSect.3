a7a625f4208ec9e6581d7fc4c36b1cb29616c39d
Aimanaqimie <152828681+Aimanaqimie@users.noreply.github.com>
2024-01-15 14:32:36 +0800

    Update contactus.js

--- contactus.js
+++ contactus.js
@@ -1,3 +1,17 @@
 function callUs() {
-    alert("You have successfully submitted a complaint! Thank you for your feedback!");
+    var name = document.getElementById("username").value;
+    var phone = document.getElementById("phone").value;
+    var complaints = document.getElementById("complaints").value;
+
+    if (name !== "" && phone !== "" && complaints !== "") {
+        window.alert("You have successfully submitted a complaint! Thank you for your feedback!");
+
+        console.log("Redirecting to contactus.html");
+        window.location.href = 'contactus.html';
+        
+    } else {
+        alert("Error! Please fill in all the fields in the form.");
+    }
+
+    
 }
